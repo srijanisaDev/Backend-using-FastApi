0e586301aa87c04a2d3f2d8983beed311d0a5793
srijanisaDev <srijankumar770@gmail.com>
2026-06-22 19:25:31 +0530

    revoking token using Redis

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -3,6 +3,7 @@ from fastapi import Request , status
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
+from src.db.redis import token_in_blocklist
 
 class TokenBearer(HTTPBearer):
     
@@ -17,8 +18,22 @@ class TokenBearer(HTTPBearer):
         if not self.token_valid(token):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="Invalid or expired token"
+                detail= {
+                    "error" : "This token is invalid or Expired",
+                    "resolution" : "Please get new token"
+                }
             )
+        
+        if await token_in_blocklist(token_data['jti']):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={
+                    "error" : "This token is invalid or has been revoked",
+                    "resolution" : "please get new token"
+                }
+                    
+                )
+                        
         
         
         self.verify_token_data(token_data)
@@ -28,7 +43,7 @@ class TokenBearer(HTTPBearer):
 
     def token_valid(self,token :str) -> bool:
         token_data = decode_token(token)
-        return True if token_data is not None else False
+        return token_data is not None 
     
     
     def verify_token_data(self,token_data):
--- src/auth/routes.py
+++ src/auth/routes.py
@@ -7,7 +7,8 @@ from fastapi.exceptions import HTTPException
 from .utils import create_access_token ,decode_token , verify_password
 from datetime import timedelta ,datetime
 from fastapi.responses import JSONResponse
-from .dependencies import RefreshTokenBearer
+from .dependencies import RefreshTokenBearer ,AccessTokenBearer
+from src.db.redis import add_jti_to_blocklist
 
 auth_router =APIRouter()
 user_service = UserService()
@@ -88,5 +89,19 @@ async def get_new_access_token(token_details:dict = Depends(RefreshTokenBearer()
         })
     
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail="Invalid or expired token")
+
+
+@auth_router.get('/logout')
+async def revoke_token(token_details:dict=Depends(AccessTokenBearer())):
+    jti = token_details['jti']
+    await add_jti_to_blocklist(jti)
+
+    return JSONResponse(
+        content={
+            "message" : "Logged out Successfully"
+        },
+        status_code=status.HTTP_200_OK
+    )
+
 
 
